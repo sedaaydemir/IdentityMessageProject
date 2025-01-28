15415cb7c48c47840db5b07405d4c47a86ce455b
Seda Aydemir <seda.aydemir7@gmail.com>
2025-01-28 21:20:00 +0300

    ...

--- IdentityMessageProject.PresentationLayer/Controllers/RegisterController.cs
+++ IdentityMessageProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityMessageProject.EntityLayer.Concrete;
+using IdentityMessageProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityMessageProject.PresentationLayer.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        //nesne ornegı ıcın constraction yaptık
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterViewModel model)
+        {
+          AppUser appuser = new AppUser()
+            {
+              
+                UserName = model.Name,
+                Email= model.Email,
+                UserSurname = model.Surname,
+                //Username = model.Username
+            };
+            var result =await _userManager.CreateAsync(appuser, model.Password);
+            if (result.Succeeded) {
+                return RedirectToAction("Index", "Login");
+            }
             return View();
         }
     }
---
+++ IdentityMessageProject.PresentationLayer/Models/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityMessageProject.PresentationLayer.Models
+{
+    public class RegisterViewModel
+    {
+        //bunu yapmaya ıhtıyac duyma sebebi:
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
